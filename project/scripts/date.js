f20880b1b5d24eba8307589a02ec1039090d5757
attahcollinso <attahcollinso@gmail.com>
2024-12-18 21:57:11 +0100

    week06 Project Assignment

---
+++ project/scripts/date.js
@@ -0,0 +1,3 @@
+let d = new Date();
+document.querySelector("#copyright").innerHTML = `&copy;${d.getFullYear()}`;
+document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`;
